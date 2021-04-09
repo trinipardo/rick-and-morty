07654bad1951e64ddd3163b9016d69f185d43e11
Trinidad Pardo <trinipardo@hotmail.com>
2021-04-09 12:30:47 -0300

    tarjtas

--- src/componentes/Container.js
+++ src/componentes/Container.js
@@ -1,11 +1,26 @@
 import Tarjetas from './Tarjetas'
 import arrayTarjetas from '../rickandmorty.json'
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+
 class Container extends Component{
+    
+    
+
+    entratTarjeta(event){
+        console.log("El mouse entro a la Tarjeta")
+        console.log(event)
+    }
+    
+    salirTarjeta(){
+
+    }
+    
     render(){
     return (
         
-        <div className="unaTarjeta-1">
+        <div className="unaTarjeta-1" onClick={this.CambiarColor}
+        onMouseEnter={this.entratTarjeta}
+        onMouseLeave={this.salirTarjeta}>
             {
                 arrayTarjetas.map(function(unaTarjeta,idx){
                     return(
--- src/componentes/Tarjetas.js
+++ src/componentes/Tarjetas.js
@@ -1,17 +1,50 @@
+import { render } from '@testing-library/react';
 import React, { Component } from 'react';
-// import arrayTarjetas from '../rickandmorty.json'
+ import arrayTarjetas from '../rickandmorty.json'
 
-function Tarjetas(props) {
-  console.log(props);
-    return (
-      <div className="tarjeta">
-        <h2>{props.mostrarTarjeta.name}</h2>
-        <img src={props.mostrarTarjeta.image} alt=""/>
-        <p>Estatus: {props.mostrarTarjeta.status}</p>
-        <p>Especies: {props.mostrarTarjeta.species}</p>
-        <p>Origin: {props.mostrarTarjeta.origin.name}</p>
-      </div>
-    )}
+ class Tarjetas extends Component {
+
+  constructor(props){
+    super(props);
+    this.state= {
+      color: this.props.color,
+      colorOriginal: this.props.color
+    }
+  }
+ }
+
+ CambiarColor= (nuevoColor)=>{
+   if (this.state.color == this.setState.colorOriginal)
+   this.setState({color: nuevoColor});
+
+}
+
+render() {
+  const { image, name, origin, status,  species } = this.props;
+
+  return (
+          <div className="tarjeta" style={{backgroundColor:  this.state.color}}>
+          <h2>{name}</h2>
+          <img src={image} alt=""/>
+          <p>Estatus: {status}</p>
+           <p>Especies: {species}</p>
+            <p>Origin: {origin}</p>
+          </div>
+       )
+        }
+
+
+ // function Tarjetas(props) {
+//   console.log(props);
+//     return (
+//       <div className="tarjeta">
+//         <h2>{props.mostrarTarjeta.name}</h2>
+//         <img src={props.mostrarTarjeta.image} alt=""/>
+//         <p>Estatus: {props.mostrarTarjeta.status}</p>
+//         <p>Especies: {props.mostrarTarjeta.species}</p>
+//         <p>Origin: {props.mostrarTarjeta.origin.name}</p>
+//       </div>
+//    )} 
 
 
 export default Tarjetas;
